99f2ca0af20568efa37228c7bc89dc0ed4423b6c
R00tB33rMan <36140389+R00tB33rMan@users.noreply.github.com>
2023-09-26 16:22:42 +0000

    Fix OptiFine's Connected Textures in 1.19.3+ & Fix compatibility regression with Fabric Loader

--- src/main/java/me/modmuss50/optifabric/mod/OptifabricSetup.java
+++ src/main/java/me/modmuss50/optifabric/mod/OptifabricSetup.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 
 import com.google.common.base.MoreObjects;
 
+import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import org.apache.commons.lang3.tuple.Pair;
 
 import org.apache.logging.log4j.LogManager;
@@ -53,6 +54,7 @@ public class OptifabricSetup implements Runnable {
 	public void run() {
 		MixinInternals.registerExtension(new MixinFixerExtension());
 		Mixins.registerErrorHandlerClass(OptifabricMixinErrorHandler.class.getName());
+		MixinExtrasBootstrap.init();
 
 		OptifineInjector injector;
 		try {
